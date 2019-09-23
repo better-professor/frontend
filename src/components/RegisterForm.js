a3ad3eb4ddc26e7b5e237c3dd56f27e45cc918ee
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-09-23 23:28:14 +0300

    added a field to RegisterFrom.js

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,10 +1,68 @@
 import React from "react";
+import axiosWithAuth from "../utils/axiosWithAuth"
 
-const RegisterForm = () => {
+class LoginForm extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      lastname:'',
+      password: ''
+    }
+  };
 
+  // make a post request to retrieve a token from the api
+  // when you have handled the token, navigate to the StudentList route
+
+  login = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post('http://localhost:5000/api/login', this.state.credentials)
+      .then(res => {
+        console.log(this.props.history)
+        localStorage.setItem('token', res.data.payload);
+        this.props.history.push('/protected');
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  render() {
     return (
-        <div>Hello from Register form</div>
-    )
-}
+      <>
+        <h1>Welcome to the Better Professor App!</h1>
+        
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="lastname"
+            value={this.state.credentials.lastname}
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </>
+    ); 
+  }
+};
 
-export default RegisterForm;
+export default LoginForm;
